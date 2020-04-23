c889c23e1705f0ebc0064947aa5f53cf7708e25e
braveadmin <adria.bravo94@gmail.com>
2020-04-23 08:20:14 +0000

    rails example with docker compose

---
+++ flask-composer/app.py
@@ -0,0 +1,31 @@
+import time
+
+import redis
+from flask import Flask
+
+import os
+
+# Set environment variables
+#REDIS_HOST = os.environ['REDIS_HOST']
+#REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
+
+app = Flask(__name__)
+cache = redis.Redis(host='redis', port=6379)
+
+
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
+
+
+@app.route('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello Hsbl! I have been seen {} times.\n'.format(count)
